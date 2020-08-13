5cfea71cbb9f0a42dd3a7784bb94a66d01826901
Marzena Bortnowska-Zavala <marzenazavala@gmail.com>
2020-08-13 15:26:54 +0200

    home and basic setup

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './home.scss';
+
+const Home = () => {
+    return(
+        <div className='home-container'>
+            <div className='box-container'>
+                <div className='box box-1'>js</div>
+                <div className='box box-2'>react</div>
+                <div className='box box-3'>react</div>
+                <div className='box box-4'>js</div>
+                <div className='box box-5'>cv</div>
+                <div className='box box-6'>me</div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Home
